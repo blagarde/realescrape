62423d4ed8e7c118250caa700394f1c22584f8e0
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2014-10-26 11:45:02 +0100

    ADD: scrappy project

---
+++ relist/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for relist project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'relist'
+
+SPIDER_MODULES = ['relist.spiders']
+NEWSPIDER_MODULE = 'relist.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'relist (+http://www.yourdomain.com)'
